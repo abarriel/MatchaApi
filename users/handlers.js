e9f1b99adcb9f923f0e3efcfb52e4238e3f1fa48
abarriel <abarriel@student.42.fr>
2017-05-15 01:55:04 +0200

    catching error

--- users/handlers.js
+++ users/handlers.js
@@ -1,17 +1,18 @@
 import debug from 'debug';
 import * as parse from './parsers';
+import prepareQueryRegister from './register';
 
 const logger = debug('matcha:users/handlers.js:');
-
+// Users already Register using the same Email and Login
 export const register = (req, res, next) => {
   const errorParser = parse.register(req.body);
   if (errorParser) throw errorParser;
   delete req.body.repassword;
-  const foundUsers = req.dUsers.findOne(
-    {
-      $or: [{ login: req.body.login }, { email: req.body.email }],
-    });
-  logger(foundUsers);
+  req.dUsers
+  .findOne({ $or: [{ login: 'req.body.login' }, { email: 'req.body.email' }] })
+  .then((err) => { if (err) throw err; })
+  .catch(() => { logger('users already Register using the same Email and Login'); })
+  .then(prepareQueryRegister);
   res.send();
   return next();
 };
---
+++ users/register.js
@@ -0,0 +1,11 @@
+import debug from 'debug';
+
+const logger = debug('matcha:register.js');
+
+export const prepareQueryRegister = (res, req) => {
+  logger(req);
+};
+
+export const prepareQuerLogin = (res, req) => {
+  logger(req);
+};

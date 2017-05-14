94ba3980ebe6f1f39ca6eb6e5702704e54340b41
abarriel <abarriel@student.42.fr>
2017-05-14 04:28:19 +0200

    need to handles true and false

--- users/validators.js
+++ users/validators.js
@@ -10,38 +10,38 @@ function isEmpty(string) {
 function isSpecialChar(string) {
   return (/\W+/.test(string));
 }
-// match all post which the expected one
+
+// Match all post which the expected one didn't catch the err yet
 const postCheckEmptyAndKey = (fixedElm, body) => {
   const error = [];
   _.forEach(body, (val, key) => {
-    if (typeof val === 'object') { return error.push('more than one values'); }
-    if (typeof val === 'object') { return error.push('not good '); }
-    if (fixedElm.indexOf(key) === -1) { return error.push('not good action'); }
+    if (typeof val === 'object') { return error.push('More than one value'); }
+    if (fixedElm.indexOf(key) === -1) { return error.push('Unknown post method'); }
     return (true);
   });
   _.forEach(fixedElm, (val) => {
-    if (body[val] === undefined || isEmpty(body[val])) { return error.push('empty values'); }
+    if (body[val] === undefined || isEmpty(body[val])) { return error.push('Empty values'); }
     return (true);
   });
-  logger(`Error = ${error}`);
-  // logger(`Values = ${fixedElm}\n`);
+  // logger(`Error = ${error}`);
+  if (Object.keys(error).length === 0) return (false);
+  return (true);
 };
 
 const mail = (email) => {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!re.test(email)) return (logger('Email is Invalid'));
-  return (logger('valid email'));
+  if (!re.test(email)) return (false);
+  return (true);
 };
 
 const name = (nom) => {
-  logger(nom);
-  if (!/^[A-Za-z ]{2,30}$/.test(nom)) return (logger('Name has a special Chars'));
-  return (logger('good name'));
+  if (!/^[A-Za-z ]{2,30}$/.test(nom)) return (false);
+  return (true);
 };
 
 const password = (passwd) => {
-  if (!/^(?=.*[a-zA-Z])(?=.*\W)(?=.*[0-9]).{6,}$/.test(passwd)) return (logger('Not Good Password'));
-  return (logger('good password'));
+  if (!/^(?=.*[a-zA-Z])(?=.*\W)(?=.*[0-9]).{6,}$/.test(passwd)) return (false);
+  return (true);
 };
 
 const login = (username) => {

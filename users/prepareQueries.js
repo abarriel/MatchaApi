318053776d049a5abaf2ebb4a301f49b9dfdd82e
Allan Barrielle <AllanBarrielle@macbook-pro-de-allan.home>
2017-05-16 05:05:05 +0200

    jwt start

--- users/prepareQueries.js
+++ users/prepareQueries.js
@@ -5,19 +5,13 @@ const logger = debug('matcha:prepareQueryRegister.js');
 
 export const prepareQueryRegister = (req) => {
   cryptPassword(req.body.password)
-  .then((pass) => {
+  .then(((pass) => {
     req.body.password = pass;
     req.dUsers
     .insert(req.body)
-    .catch(() => { logger('insert failed'); })
-    .then(
-      jwt.sign({ foo: 'bar' }, "cer", { algorithm: 'RS256' }, function(err, token) {
-  console.log(token);
-})
-);
-  }).catch(() => { logger('password error'); });
+    .catch(() => { logger('insert failed'); });
+  })).catch(() => { logger('password error'); });
 };
-
 export const prepareQuerLogin = (req, res) => {
   logger(res);
 };
